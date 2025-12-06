51c102e919e380430d10c9b2ade960184c90da06
felltrin <luciholzinger@gmail.com>
2025-12-06 02:00:34 -0800

    [TR-6] The top left tile is being drawn to the screen

--- src/maze.cpp
+++ src/maze.cpp
@@ -76,41 +76,66 @@ void Maze::Draw()
     {
         for (int j = 0; j < numCols; j++)
         {
-            Vector2 center = {(float)(j * numberTwo + (0.5 * numberTwo)), (float)(i * numberOne + (0.5 * numberOne))};
+            // Vector2 center = {(float)(j * numberTwo + (0.5 * numberTwo)), (float)(i * numberOne + (0.5 * numberOne))};
+            Vector2 center;
             Vector2 startPos;
             Vector2 endPos;
+
+            Rectangle rect;
+            float radiusX;
+            float radiusY;
+            float startAngleDeg;
+            float endAngleDeg;
+
             switch (maze[i][j])
             {
             case 1:
+            {
+                center = {(float)(j * numberTwo + (0.5 * numberTwo)), (float)(i * numberOne + (0.5 * numberOne))};
                 DrawCircleV(center, 4, RAYWHITE);
                 break;
+            }
+
             case 2:
+            {
+                center = {(float)(j * numberTwo + (0.5 * numberTwo)), (float)(i * numberOne + (0.5 * numberOne))};
                 DrawCircleV(center, 10, RAYWHITE);
                 break;
+            }
+
             case 3:
+            {
+                center = {(float)(j * numberTwo + (0.5 * numberTwo)), (float)(i * numberOne + (0.5 * numberOne))};
                 startPos = {center.x, (float)(i * numberOne)};
                 endPos = {center.x, (float)(i * numberOne + numberOne)};
                 DrawLineEx(startPos, endPos, 3, BLUE);
                 break;
+            }
+
             case 4:
+            {
+                center = {(float)(j * numberTwo + (0.5 * numberTwo)), (float)(i * numberOne + (0.5 * numberOne))};
                 startPos = {(float)(j * numberTwo), center.y};
                 endPos = {(float)(j * numberTwo + numberTwo), center.y};
                 DrawLineEx(startPos, endPos, 3, BLUE);
                 break;
+            }
+
             case 5:
+            {
                 // Calculate center and radii from the rectangle
-                Rectangle rect = {
+                rect = {
                     (float)((j * numberTwo - (numberTwo * 0.4)) - 2),
                     (float)(i * numberOne + (0.5f * numberOne)),
                     (float)numberTwo,
                     (float)numberOne};
 
-                Vector2 center = {rect.x + rect.width / 2, rect.y + rect.height / 2};
-                float radiusX = rect.width / 2.0f;
-                float radiusY = rect.height / 2.0f;
+                center = {rect.x + rect.width / 2, rect.y + rect.height / 2};
+                radiusX = rect.width / 2.0f;
+                radiusY = rect.height / 2.0f;
 
-                float startAngleDeg = (3 * (PI / 2.0f)) * RAD2DEG;
-                float endAngleDeg = (PI * 2.0f) * RAD2DEG;
+                startAngleDeg = (3 * (PI / 2.0f)) * RAD2DEG;
+                endAngleDeg = (PI * 2.0f) * RAD2DEG;
 
                 DrawRing(
                     {center.x, center.y},
@@ -121,9 +146,39 @@ void Maze::Draw()
                     600, // segments (increase for smoother arc)
                     BLUE);
                 break;
-                // default:
-                //     std::cout << "lol!" << std::endl;
-                //     break;
+            }
+
+            case 6:
+            {
+                // Calculate center and radii from the rectangle
+                rect = {
+                    (float)((j * numberTwo + (numberTwo * 0.5))),
+                    (float)(i * numberOne + (0.5f * numberOne)),
+                    (float)numberTwo,
+                    (float)numberOne};
+
+                center = {rect.x + rect.width / 2, rect.y + rect.height / 2};
+                radiusX = rect.width / 2.0f;
+                radiusY = rect.height / 2.0f;
+
+                startAngleDeg = (PI)*RAD2DEG;
+                endAngleDeg = (3 * (PI / 2.0f)) * RAD2DEG;
+
+                DrawRing(
+                    {center.x, center.y},
+                    5,
+                    10,
+                    startAngleDeg,
+                    endAngleDeg,
+                    600, // segments (increase for smoother arc)
+                    BLUE);
+                break;
+            }
+            default:
+            {
+                std::cout << "lol!" << std::endl;
+                break;
+            }
             }
         }
     }
